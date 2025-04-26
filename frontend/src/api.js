0e04cc4ec2f2c402c5a04ec0c665f3da7ac0a453
AbhiDevX-1 <abhinayr258@gmail.com>
2025-04-26 14:37:15 +0530

    Update API URL to point to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/students';
+const API_URL = 'https://student-management-system-backend-48to.onrender.com';
 
 // Get all students
 export const getStudents = async () => {
