1057c54d4ec5059b053cda832d90a4cde3351b4c
AbhiDevX-1 <abhinayr258@gmail.com>
2025-04-26 14:27:03 +0530

    Fix mongoose deprecated options

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true, // Ensure using the new parser
       useUnifiedTopology: true, // Ensure unified topology for better connection handling
-      useFindAndModify: false, // Avoid deprecation warnings
-      useCreateIndex: true, // Ensure indexing is handled correctly
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
@@ -19,4 +17,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-    
